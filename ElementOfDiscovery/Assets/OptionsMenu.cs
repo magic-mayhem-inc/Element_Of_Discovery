6aebb996ddf7edf3714e668b7f4b814bbb619a34
Ivana-Pagan <37190657+Ivana-Pagan@users.noreply.github.com>
2019-03-25 16:26:58 -0400

    Better Fire
    Fixed the lava collider, and programed most of the UI

---
+++ ElementOfDiscovery/Assets/OptionsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+   
+
+
+    public void BackButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
+
